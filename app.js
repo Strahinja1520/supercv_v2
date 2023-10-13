b2f5ebd09a884a0fb089f5cf87376f4be38354e5
Strahinja1520 <138989667+Strahinja1520@users.noreply.github.com>
2023-10-13 22:11:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,22 @@ menu.addEventListener("click", (e) => {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+function SendMail(){
+  var params = {
+    from_name: document.getElementById("fullName").value,
+    email_id: document.getElementById("email_id").value,
+    message: document.getElementById("message").value
+  };
+
+  emailjs.send('service_s4ie1ap', 'template_39ucqeh', params)
+  .then(function(response) {
+     console.log('SUCCESS!', response.status, response.text);
+  }, function(error) {
+     console.log('FAILED...', error);
+  });
+
+  document.getElementById("fullName").value = "";
+  document.getElementById("email_id").value = "";
+  document.getElementById("message").value = "";
+}
